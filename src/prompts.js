8faea0ffd51586e18e2cbab0afe299f7feec5ac5
Dennis Mayer <19yingyang88@gmail.com>
2024-05-19 16:14:44 +0200

    Initial commit

--- src/prompts.js
+++ src/prompts.js
@@ -1,5 +1,4 @@
-export const characterGeneratorSystemPrompt = `You're a character generator for a game. You will get requests to generate a character, and you will respond with a JSON character object with the following structure:
-{
+export const characterStructure = `{
   firstName: string;
   surname: string;
   gender: string;
@@ -20,32 +19,14 @@ export const characterGeneratorSystemPrompt = `You're a character generator for 
 
   mood: string;
   interests: string[];
-}
+}`;
+
+export const characterGeneratorSystemPrompt = `You're a character generator for a game. You will get requests to generate a character, and you will respond with a JSON character object with the following structure:
+${characterStructure}
 You only respond with the generated JSON, nothing else. Respond in only one line without any line breaks. It is important that your response is actually a valid JSON.`;
 
 export const systemPromptGeneratorSystemPrompt = `You are a character system prompt generator for a game. You will get a character definition with following structure:
-{
-  firstName: string;
-  surname: string;
-  gender: string;
-  age: number;
-  occupation: string;
-
-  personalityType: string;
-  traits: {
-    honest: boolean;
-    trustworthy: boolean;
-    [key: string]: boolean; // custom traits
-  };
-
-  topicsOfInterest: string[];
-  responses: {
-    [topicOfInterest: string]: string[];
-  };
-
-  mood: string;
-  interests: string[];
-}
+${characterStructure}
 You will respond with a message to the character that describes himself and how he will speak with someone else (You are ...). Only respond with the generated prompt, nothing else.`;
 
 export const greetingMessageGeneratorSystemPrompt = `You are a greeting message generator for a game. 
